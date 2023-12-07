4e91596c7c7ba6d1263de917ecd54c25284c25c2
Connor <connor.lofink@gmail.com>
2023-12-07 01:05:50 -0500

    Slightly cleaned the code up. No logic changes

--- Day7Solution2.js
+++ Day7Solution2.js
@@ -7,86 +7,65 @@ function day7Solution2(data) {
         handInfo.hand = line.split(" ")[0];
         handInfo.bid = parseInt(line.split(" ")[1], 10);
         const hand = {};
-        let count2s
-        let count3s
-        let count4s
-        let count5s
-        let countJs
 
-        if (handInfo.hand === "JJJJJ") {
-            count2s = 0
-            count3s = 0
-            count4s = 0
-            count5s = 1
-            countJs = 5
+        for (let char of handInfo.hand) {
+            if (char === "J") continue;
+            if (!hand[char]) hand[char] = 0;
+            hand[char]++;
         }
-        else {
-            for (let char of handInfo.hand) {
-                if (char === "J") continue;
-                if (!hand[char]) hand[char] = 0;
-                hand[char]++;
-            }
-            const counts = Object.values(hand)
-            count2s = counts.join("").split("2").length - 1 || 0;
-            count3s = counts.join("").split("3").length - 1 || 0;
-            count4s = counts.join("").split("4").length - 1 || 0;
-            count5s = counts.join("").split("5").length - 1 || 0;
-            countJs = handInfo.hand.split("J").length - 1 || 0;
-        }
+        const counts = Object.values(hand)
+        let count2s = counts.join("").split("2").length - 1 || 0;
+        let count3s = counts.join("").split("3").length - 1 || 0;
+        let count4s = counts.join("").split("4").length - 1 || 0;
+        let count5s = counts.join("").split("5").length - 1 || 0;
+        let countJs = handInfo.hand.split("J").length - 1 || 0;
+        
         // five of a kind
-        if (count5s === 1) {
-            hands.five.push(handInfo);
-        }
-        else if (count4s === 1 && countJs === 1) {
-            hands.five.push(handInfo);
-        }
-        else if (count3s === 1 && countJs === 2) {
-            hands.five.push(handInfo);
-        }
-        else if (count2s === 1 && countJs === 3) {
-            hands.five.push(handInfo);
-        }
-        else if (countJs === 4) {
+        if (
+            (count5s === 1) || 
+            (count4s === 1 && countJs === 1) ||
+            (count3s === 1 && countJs === 2) ||
+            (count2s === 1 && countJs === 3) ||
+            (countJs >= 4)
+        ) {
             hands.five.push(handInfo);
         }
         // four of a kind
-        else if (count4s === 1) {
-            hands.four.push(handInfo);
-        }
-        else if (count3s === 1 && countJs === 1) {
-            hands.four.push(handInfo);
-        }
-        else if (count2s === 1 && countJs === 2) {
-            hands.four.push(handInfo);
-        }
-        else if (countJs === 3) {
+        else if (
+            (count4s === 1) ||
+            (count3s === 1 && countJs === 1) ||
+            (count2s === 1 && countJs === 2) ||
+            (countJs === 3)
+        ) {
             hands.four.push(handInfo);
         }
         // full house
-        else if (count3s === 1 && count2s === 1) {
+        else if (
+            (count3s === 1 && count2s === 1) ||
+            (count2s === 2 && countJs === 1)
+        ) {
             hands.full.push(handInfo);
         }
-        else if (count2s === 2 && countJs === 1) {
-            hands.full.push(handInfo);
-        }
-        else if (count3s === 1) {
+        // three of a kind
+        else if (
+            (count3s === 1) ||
+            (count2s === 1 && countJs === 1) ||
+            (countJs === 2)
+        ) {
             hands.three.push(handInfo)
         }
-        else if (count2s === 1 && countJs === 1) {
-            hands.three.push(handInfo)
-        }
-        else if (countJs === 2) {
-            hands.three.push(handInfo);
-        }
+        // two pair
         else if (count2s === 2) {
             hands.twoPair.push(handInfo);
         }
-        else if (count2s === 1) {
+        // pair
+        else if (
+            (count2s === 1) || 
+            (countJs === 1)
+        ) {
             hands.pair.push(handInfo);
         }
-        else if (countJs === 1) {
-            hands.pair.push(handInfo);
-        }
+        // high card
         else {
             hands.high.push(handInfo)
         }
